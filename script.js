8b2d3eb491a243300b196fd3c81fcaa966ccaa48
Ilya Kashitsyn <ilyakashitsyn@gmail.com>
2025-03-27 01:42:12 -0300

    added word list and elements refs

---
+++ script.js
@@ -0,0 +1,11 @@
+const wordEl = document.getElementById('word');
+const wrongLettersEl = document.getElementById('wrong-letters');
+const playAgainBtn = document.getElementById('play-button');
+const popup = document.getElementById('popup-container');
+const notification = document.getElementById('notification-container');
+const finalMessage = document.getElementById('final-message');
+
+const figureParts = document.querySelectorAll('.figure-part');
+
+// Words
+const words = ['application', 'programming', 'interface', 'wizard'];
